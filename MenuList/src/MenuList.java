b7bd64187f8afbacef17f52afbe267c0fc284642
nchristiny <icarus747@gmail.com>
2017-09-27 13:21:47 -0500

    Refactor program2

--- MenuList/src/MenuList.java
+++ MenuList/src/MenuList.java
@@ -16,36 +16,42 @@ public class MenuList {
 		Scanner console = new Scanner(System.in); 
 		Boolean selection_valid = false;
 		String answer;
-		System.out.print("Please enter your selection >>> ");
+		System.out.println("A.\tDisplay the multiples of 3 backward from 33 to 3.\nB.\tDisplay the capital letters of the alphabet backward from Z to A.\nQ.\tExit");
 		do {
+			System.out.print("\nPlease enter your selection >>> ");
 			answer = console.next().toUpperCase();
-			if (answer.equals("A") || answer.equals("B")  || answer.equals("Q"))
+			if (answer.equals("A") || answer.equals("B")) {
 				selection_valid = true;	
+				switch(answer){
+				case "A":
+					for (int i = 33; i >= 3; i--){
+						if (i % 3 == 0)
+							System.out.print(i + " ");
+					}
+					selection_valid = false;	
+					break;
+				case "B":
+					for (int i = 90; i >= 65; i-- ){
+						char c = (char)i;
+						System.out.print(c + " ");
+					}
+					selection_valid = false;	
+					break;
+				default:
+					System.out.println("Something went wrong");
+				}
+			}
+			else if (answer.equals("Q")) {
+				System.out.println("Thank you for using this program.");
+				console.close();
+				System.exit(0);
+			}
 			else {
 				System.out.println("Choice must be 'A', 'B', or 'Q'");
 				System.out.print("Please reenter: ");
 			}
-		} while (!selection_valid);
 
-		switch(answer){
-		case "A":
-			for (int i = 0; i <= 33; i += 3){
-				System.out.print(33 - i + " ");
-			}
-			break;
-		case "B":
-			for (int i = 25; i >= 0; i-- ){
-				String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-				System.out.print(alphabet.charAt(i) + " ");
-			}
-			break;
-		case "Q":
-			System.out.println("Thank you for using this program.");
-			System.exit(0);
-		default:
-			System.out.println("Something went wrong");
-		}
-		console.close();
+		} while (!selection_valid);
 	}
 
 }
