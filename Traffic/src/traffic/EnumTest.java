d62b096941948fd371006db3c15c5714aa9ae4ba
nchristiny <icarus747@gmail.com>
2017-10-02 10:34:27 -0500

    Finalize program

--- Traffic/src/traffic/EnumTest.java
+++ Traffic/src/traffic/EnumTest.java
@@ -12,7 +12,7 @@ public class EnumTest {
 	public static void main(String[] args) {
 		// Display header of Light and Duration
 		System.out.println("Light\tDuration");
-		
+
 		// print all traffic lights and their duration using enhanced for loop
 		for(TrafficLight color : TrafficLight.values()) {
 			System.out.printf("%s\t%d%n", color, color.getSeconds());
--- Traffic/src/traffic/TrafficLight.java
+++ Traffic/src/traffic/TrafficLight.java
@@ -12,18 +12,18 @@ public enum TrafficLight {
 	RED (50),
 	GREEN (40),
 	YELLOW (5);
-	
+
 	// Instance fields (Durations of the light)
 	private final int seconds;
-	
+
 	// enum type constructor
 	TrafficLight(int seconds){
 		this.seconds = seconds;
 	}
-	
+
 	// accessor for duration (getter)
 	int getSeconds(){
 		return seconds;
 	}
-	
+
 }
